330f50d1275e8dd0abb58b984ef48addee580e56
ben.harvey1213 <ben.harvey1213@gmail.com>
2020-06-01 21:06:02 -0500

    final updates

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
 const inputBox = document.getElementById('messageInput');
 const messages = document.getElementById('messages');
 
+const demoSkip = document.getElementById('demo-skip');
+const skillsSkip = document.getElementById('skills-skip');
+const projectsSkip = document.getElementById('projects-skip');
+const educationSkip = document.getElementById('education-skip');
+const workSkip = document.getElementById('work-skip');
+const otherSkip = document.getElementById('other-skip');
+
 var forceStrings = ['Wow! This is amazing, Ben!', 'You are so great at coding!', 'You know what? You\'re hired!', 'I\'m going to contact you about that right now!'];
-var responseStrings = ['Thank you!', 'I appreciate it!!', 'Wow, when do I start??', 'Awesome, looking forward to it!'];
+var responseStrings = ['Thank you!', 'I appreciate it!!', 'Wow, when do I start??', ''];
 
 var forceIndex = 0;
 
 var currentForceString = 0;
+
+console.log('Check out my code 😊')
+
+AOS.init();
+
+demoSkip.addEventListener('click', function() {
+    window.location = 'index.html#demo-skip-section';
+    window.scrollBy(0, -30);
+});
+
+skillsSkip.addEventListener('click', function() {
+    window.location.hash = 'skills-skip-section';
+    window.scrollBy(0, -30);
+});
+
+projectsSkip.addEventListener('click', function() {
+    window.location.hash = 'projects-skip-section';
+    window.scrollBy(0, -30);
+});
+
+workSkip.addEventListener('click', function() {
+    window.location.hash = 'work-skip-section';
+    window.scrollBy(0, -30);
+});
+
+educationSkip.addEventListener('click', function() {
+    window.location.hash = 'education-skip-section';
+    window.scrollBy(0, -30);
+});
+
+otherSkip.addEventListener('click', function() {
+    window.location.hash = 'other-skip-section';
+    window.scrollBy(0, -30);
+});
+
 
 inputBox.addEventListener('keypress', forceText);
 
@@ -21,6 +63,8 @@ inputBox.addEventListener('click', function(e) {
     e.preventDefault();
     inputBox.setSelectionRange(inputBox.value.length, inputBox.value.length);
 });
+
+
 
 function forceText(e) {
     e.preventDefault();
@@ -62,17 +106,43 @@ function sendMessage(message) {
 
 function addNextResponse() {
     sleep(750).then(() => {
-        var wrapper = document.createElement('div');
-        wrapper.className = 'messages-left';
+        if (currentForceString < responseStrings.length) {
+            var wrapper = document.createElement('div');
+            wrapper.className = 'messages-left';
 
-        var node = document.createElement('div');
-        node.className = 'msg msg-left';
-        node.textContent = responseStrings[currentForceString - 1];
+            var node = document.createElement('div');
+            node.className = 'msg msg-left';
+            node.textContent = responseStrings[currentForceString - 1];
 
-        wrapper.appendChild(node);
+            wrapper.appendChild(node);
 
-        messages.appendChild(wrapper);
-        messages.scrollTop = messages.scrollHeight;
+            messages.appendChild(wrapper);
+            messages.scrollTop = messages.scrollHeight;
+        } else {
+            var wrapper = document.createElement('div');
+            wrapper.className = 'messages-left';
+
+            var node = document.createElement('div');
+            node.className = 'msg msg-left';
+            node.textContent = 'Awesome! ';
+
+            var link = document.createElement('a');
+            link.textContent = 'Shoot me an email!';
+            link.href = 'mailto:ben.harvey1213@gmail.com';
+            link.className = 'text-link';
+
+            var bold = document.createElement('b');
+            bold.appendChild(link);
+
+            node.appendChild(bold)
+
+            wrapper.appendChild(node);
+
+            messages.appendChild(wrapper);
+            messages.scrollTop = messages.scrollHeight;
+        }
+
+
     })
 }
 
